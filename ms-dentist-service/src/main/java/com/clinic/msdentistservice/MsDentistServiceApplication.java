9674dc55e8123969a04f1caf026efd303f784bd3
Grego Garcia <82552333+Dgarciadani@users.noreply.github.com>
2022-09-09 17:02:06 -0300

    [ADD] service projects

---
+++ ms-dentist-service/src/main/java/com/clinic/msdentistservice/MsDentistServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.msdentistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDentistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDentistServiceApplication.class, args);
+	}
+
+}
---
+++ ms-patient-service/src/main/java/com/clinic/mspatientservice/MsPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.mspatientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPatientServiceApplication.class, args);
+	}
+
+}
